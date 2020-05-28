80a747b34696354db3deb3f592d9cb3f45e508a5
Robert Krueckeberg <robert.krueckeberg@sinnerschrader.com>
2020-05-28 18:54:53 +0200

    update jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), __dirname],
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
