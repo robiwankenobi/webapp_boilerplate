dbe86b3dce6e258e100ddb550961f72fda8376a4
Robert Krueckeberg <robert.krueckeberg@sinnerschrader.com>
2020-05-29 12:44:12 +0200

    speed up jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
   testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'node',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
